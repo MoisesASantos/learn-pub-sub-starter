53e8e35c0bda3bcb381708b59b8b0889e1aa67fa
MoisesASantos <moiseshitalukuasantos@gmail.com>
2026-07-15 13:56:50 +0100

    sending the connection code with the rabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	
+	fmt.Println("Connected to RabbitMQ")
+
+	// Canal para receber sinais do sistema operativo
+	sigCh := make(chan os.Signal, 1)
+	// Escuta Ctrl+C (SIGINT) e SIGTERM
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	// Espera até receber um sinal
+	<-sigCh
+
+	fmt.Println("Shutting down...")
 }
