8a9878534870be6efa3e91c108cd820240608932
MoisesASantos <moiseshitalukuasantos@gmail.com>
2026-07-15 16:41:32 +0100

    add a command on client side

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -30,28 +30,58 @@ func main() {
 	elements := []string{routing.PauseKey, username}
 	queueName := strings.Join(elements, ".")
 	ch, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
-
 	if err != nil {
 		panic(err)
 	}
 	defer ch.Close()
-
 	fmt.Println("Queue declared:", queue.Name)
+	
+	gamestate := gamelogic.NewGameState(username)
+	for {
+		cmd := gamelogic.GetInput()
+
+		if len(cmd) == 0 {
+			continue
+		}
+
+		switch cmd[0] {
+
+		case "spawn":
+			_ = gamestate.CommandSpawn(cmd)
+
+		case "move":
+			_, _ = gamestate.CommandMove(cmd)
+		
+		case "status":
+			gamestate.CommandStatus() 
+		
+		case "help":
+			gamelogic.PrintClientHelp()
+
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+
+		case "quit":
+			gamelogic.PrintQuit()
+			break
+
+		default:
+			fmt.Println("I don't understand that command")
+		}
+	}
+
 
 
 	// Criar canal para sinais do sistema operativo
 	sigCh := make(chan os.Signal, 1)
-
 	// Escutar Ctrl+C e SIGTERM
 	signal.Notify(
 		sigCh,
 		os.Interrupt,
 		syscall.SIGTERM,
 	)
-
 	// Bloqueia até receber sinal
 	<-sigCh
-
 	fmt.Println("Shutting down...")
 }
 
